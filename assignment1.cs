98a7148d8eda48e2174a9c1938ff19c9ec477df8
WiktorNowak <60354506+WiktorNowak@users.noreply.github.com>
2020-02-29 20:22:13 +0000

    Add files via upload

---
+++ assignment1.cs
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<!-- The following lines are comments
+	Author : Wiktor Nowak
+	Date created: 30/01/2020
+	Page Description: Home page of Comapny sales website
+	Modified 30/01/2020 Created Copyright-->
+	
+<html lang="en"> <!-- Declare the primary language for each web page-->
+<head>
+	<meta charset="utf-8" /> <!--8-bit character encoding capable of encoding all possible characters-->
+	<meta name="description" content="Dreamweaver CC 2014 Book">
+	<meta name="keywords" content="Book, Dreamweaver, Skills">
+	<meta name="author" content="Wiktor Nowak">
+    <title>Dreamweaver book</title>
+	<link rel="stylesheet" type="text/css" href="Styles/mainStyles.css"/>
+</head>
+<body> <!-- Body tag start of body section display on screen -->
+
+	<header>
+        <img style ="float: left; heightL 50px; width:50px;" src="images/logo.png" alt"logo">
+        <h1>Foodmarket Sligo</h1>
+	</header>
+    
+    <nav>
+        <ul>
+            <li id="active" <a href="index.html">Home</a></li>
+            <li> <a href="#">Menu</a></li>
+            <li> <a href="#">VideoTour</a></li>
+            <li> <a href="#">Sounds of Food</a></li>
+        </ul>    
+    </nav>
+
+    <!-- suggestion #content 80% nad float to left-->
+    <section id="content">
+        <section id="aboutus" >
+            <h2>About Us</h2>
+            <!--suggestion .showimages float to right-->
+            <img src="images/front.jpg" id="bigPic" />
+            
+	<main> <!-- Main wrapper tag that goes around your min content-->    
+       
+	</main>
+	<footer> <!-- Footer grouping of content for the bottom of the screen will need css to place it at the bottom -->
+       
+	</footer>
+</body>
+</html>
